b016a44a35bcfaca7bcd196fdd2f3ea03eaece2e
Sarfaraz Alam <71469354+sarfarazstark@users.noreply.github.com>
2021-11-15 20:27:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ START_BTN = ikb(
             ("📚 Help", "help"),
         ],
         [
-            ("👨‍💻 Developer", "https://bio.link/aminesoukara", "url"),
+            ("👨‍💻 Developer", "https://t.me/sarfarazstark", "url"),
             ("❌", "close"),
         ],
     ]
