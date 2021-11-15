d383e96df7afedc4fae769d358aeb8d42d9cbf69
Sarfaraz Alam <71469354+sarfarazstark@users.noreply.github.com>
2021-11-15 20:42:46 +0530

    Update configs.py

--- utils/configs.py
+++ utils/configs.py
@@ -20,8 +20,8 @@ class Var(object):
     # You Can Get An API Key From https://api.imgbb.com.
     API = os.environ.get("API", None)
 
-    OWNER_ID = int(os.environ.get("OWNER_ID", "853393439"))
-    BOT_NAME = os.environ.get("BOT_NAME", "ImgBBRobot")
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1120608892"))
+    BOT_NAME = os.environ.get("BOT_NAME", "ImgbBot")
 
     START_PIC = "https://i.imgur.com/zYIllxt.jpg"
     HELP_PIC = "https://i.imgur.com/AmxAlix.jpg"
@@ -30,40 +30,38 @@ class Var(object):
 class Tr(object):
 
     START_TEXT = """
-👋 Hi ! {} Welcome To @ImgBBRobot
+👋 Hi ! {} Welcome To @ImgbBot
 
 **With This Bot You Can Hosts Your Images On imgbb.com **
 
 You Can Send An Image As Forwarded Message From Any Chat/Channel Or Upload It As Photo Or File.
 """
 
-    ABOUT_TEXT = """🤖 **My Name:** [ImgBB](t.me/ImgBBRobot)
+    ABOUT_TEXT = """🤖 **My Name:** [ImgBB](t.me/ImgbBot)
 
 📝 **Language:** [Python 3](https://www.python.org)
 
 📚 **Framework:** [Pyrogram](https://github.com/pyrogram/pyrogram)
 
-📡 **Hosted On:** [Railway](railway.app)
+📡 **Hosted On:** [Heroku](heroku.com)
 
-👨‍💻 **Developer:** [Amine Soukara](t.me/AmineSoukara)
+👨‍💻 **Developer:** [Sarfaraz Stark](t.me/SarfarazStark)
 
-💡 **Source Code:** [Github](https://github.com/AmineSoukara/ImgBB-Bot/fork)
+👥 **Support Group:** [Blue Whale Group](https://t.me/bluewhalegroup)
 
-👥 **Support Group:** [Damien Help](https://t.me/DamienHelp)
-
-📢 **Updates Channel:** [Damien Soukara](https://t.me/DamienSoukara)
+📢 **Updates Channel:** [🐋Blue Whale Bots ](https://t.me/BlueWhaleBots)
 
 
-❤ [Donate](https://www.paypal.me/AmineSoukara) (PayPal)
+❤ [Donate](https://www.paypal.me/SarfarazStark) (PayPal)
 """
 
     HELP_TEXT = """💡 Just Send Me Your Photo And I'll Upload it To You .  That's it
 
-❤ [Donate](https://www.paypal.me/AmineSoukara) (PayPal)
+❤ [Donate](https://www.paypal.me/SarfarazStark) (PayPal)
 """
 
     ERR_TEXT = "⚠️ API Not Found"
 
-    ERRTOKEN_TEXT = "😶 The Access Token Provided Is Expired, Revoked, Malformed Or Invalid For Other Reasons. DM @AmineSoukara",
+    ERRTOKEN_TEXT = "😶 The Access Token Provided Is Expired, Revoked, Malformed Or Invalid For Other Reasons. DM @SarfarazStarkBot",
 
     WAIT = "💬 Please Wait !!"
